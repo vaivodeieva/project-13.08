0296babfc8f5fc516aa61bb11fbc2b73bb5c69c6
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-16 15:46:56 +0300

    update App.js, App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Views/Home';
+
+
 
 function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+
+    return (
+        <div>
+            <Router>
+                <Header />
+                <Switch>
+
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+                <Footer />
+            </Router>
+
+            
+        </div>
+    );
 }
 
 export default App;
