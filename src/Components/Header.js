585ad79b83795125362af409e7a3bc27aa9467df
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-16 18:22:06 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,6 @@ function Header() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -14,10 +13,13 @@ function Header() {
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="#">Github</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Hobbies</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
         </li>
       </ul>
     </div>
