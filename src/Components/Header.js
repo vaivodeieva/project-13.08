313a4dbfe4824b9fe40efce1a4a32fccb528b288
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-16 18:51:53 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,14 @@
 function Header() {
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div >
+            <nav className="navbar navbar-expand-lg navbar-light bg-light position-absolute top-50 start-50 translate-middle">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                        <ul className="navbar-nav text-center">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
