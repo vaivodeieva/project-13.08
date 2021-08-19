f52559333441b263ceb56f65b1abac3e04f9fac4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-19 15:55:33 +0300

    add content text to Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,11 +4,21 @@ function Home() {
     return (
         <div>
 
-            <div>
-                <h1 className="myName">
+                <h1 className="myName text-center">
                 IEVA VAIVODE
                 </h1>
-            </div>
+
+                <p>
+                    Welcome to my portfolio! 
+                </p>
+                <p>
+                    I'm a web developer with an art &amp; translations background. 
+                    Now I translate ideas into the code to create artworks of code.
+                </p>
+                <p>
+                    Get inspired!
+                </p>
+           
             
 
         </div>
