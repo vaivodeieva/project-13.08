c4f34757647abf044a5eb6c7e5601c9cc757410d
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-20 19:44:10 +0300

    add second button-background - for letter ''E''

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -5,12 +5,20 @@ function Home() {
         <div className="container-home">
 
                 <h1 className="myName text-center">
-                IEVA VAIV
+                I
+                <span className="text-center">
+                 <button className="text-center btn-home">
+                    <span>E</span>
+                    <div className="get-inspired white">homE</div>
+
+                </button>
+                 </span>
+                VA VAIV
                 <span className="text-center">
                  <button className="text-center btn-home">
                     <span>O</span>
-                    <div className="get-inspired">Get</div>
-                    <div className="get-inspired">inspired</div>
+                    <div className="get-inspired white">My</div>
+                    <div className="get-inspired white">wOrks</div>
 
                 </button>
                  </span>
