85ef27331979ad6f37b3f34c7f9f7f18b2014a4d
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-20 19:52:54 +0300

    update styles

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -5,39 +5,7 @@ function Home() {
         <div className="container-home">
 
                 <h1 className="myName text-center">
-                I
-                <span className="text-center">
-                 <button className="text-center btn-home">
-                    <span>E</span>
-                    <div className="get-inspired white">Home</div>
-
-                </button>
-                 </span>
-                VA V
-                <span className="text-center">
-                 <button className="text-center btn-home">
-                    <span>A</span>
-                    <div className="get-inspired white">Contact</div>
-
-                </button>
-                 </span>
-                <span className="text-center">
-                 <button className="text-center btn-home">
-                    <span>I</span>
-                    <div className="get-inspired white">Github</div>
-
-                </button>
-                 </span>
-                V
-                <span className="text-center">
-                 <button className="text-center btn-home">
-                    <span>O</span>
-                    <div className="get-inspired white">My</div>
-                    <div className="get-inspired white">wOrks</div>
-
-                </button>
-                 </span>
-                DE
+                IEVA VAIVODE
                 </h1>
 
                 <p className="text-center">
@@ -48,7 +16,11 @@ function Home() {
                     Now I translate ideas into the code to create artworks of code.
                 </p>
 
-                 
+                 <div className="text-center">
+                 <button className="text-center btn-home">
+                    GET INSPIRED
+                </button>
+                 </div>
                
            
             
