761e6efdf7848001ab4d5d8d2afcc437721fc398
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-08-24 14:41:19 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header() {
 
     return (
         <div >
-            <nav className="navbar navbar-expand-lg navbar-light bg-success">
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
