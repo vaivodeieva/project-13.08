e6a6d0b97c238febd367d23783ecfc0d5e007eee
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-01 22:47:28 +0300

    update style for Page404

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,95 +1,25 @@
-import { tsParticles } from "tsparticles";
 import '../Assets/Css/Page404.css';
 
 function Page404() {
 
-    tsParticles.load("tsparticles", {
-        particles: {
-            number: {
-                value: 100
-            },
-            move: {
-                enable: true
-            },
-            color: {
-                value: "random",
-                animation: {
-                    enable: true,
-                    speed: 40,
-                    sync: true
-                }
-            },
-
-            shape: {
-                type: "circle",
-            },
-
-            size: {
-                value: 10,
-                random: true,
-                animation: {
-                    enable: true,
-                    speed: 20,
-                    minimumValue: 0.5,
-                    sync: false
-                }
-            },
-
-            links: {
-                enable: true,
-                distance: 100,
-                color: "#fff",
-                opacity: 0.6,
-                width: 1
-            },
-
-        },
-        interactivity: {
-            detectsOn: "canvas",
-            events: {
-                onHover: {
-                    enable: true,
-                    mode: "repulse"
-                },
-                onClick: {
-                    enable: true,
-                    mode: "bubble"
-                },
-                resize: true
-            },
-            modes: {
-                grab: {
-                    distance: 100,
-                    links: {
-                        opacity: 1
-                    }
-                },
-                bubble: {
-                    distance: 200,
-                    size: 40,
-                    duration: 2,
-                    opacity: 0.8
-                },
-                repulse: {
-                    distance: 100
-                },
-                push: {
-                    quantity: 2
-                },
-                remove: {
-                    quantity: 2
-                }
-            }
-        },
-        detectRetina: true,
-
-
-    });
-
     return (
-        <div id="tsparticles">
-            
-            
+        <div>
+            <div id="clouds">
+            <div className="cloud x1"></div>
+            <div className="cloud x1_5"></div>
+            <div className="cloud x2"></div>
+            <div className="cloud x3"></div>
+            <div className="cloud x4"></div>
+            <div className="cloud x5"></div>
+        </div>
+        <div className='c'>
+            <div className='_404'>404</div>
+            <hr/>
+            <div className='_1'>THE PAGE</div>
+            <div className='_2'>WAS NOT FOUND</div>
+            <a className='btn' href='/'>BACK TO MARS</a>
+        </div>
+
         </div>
     )
 }
