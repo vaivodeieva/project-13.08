e9ec032e88813c7dcfdccae391a8682a4419f672
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-01 09:58:09 +0300

    update style for Contact.js, Header.js

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -2,10 +2,8 @@ import "../Assets/Css/Contact.css";
 
 function Contact() {
     return (
-        <div>
-            <textarea>
-                text
-            </textarea>
+        <div className="contact-container">
+            
         </div>
     )
 }
