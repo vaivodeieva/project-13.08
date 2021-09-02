9c6ccb1b5310c96ee3fc96869dc185b1cc7138bc
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-02 21:23:35 +0300

    update style for page404

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -4,20 +4,20 @@ function Page404() {
 
     return (
         <div className="display404">
-            <div id="clouds">
-            <div className="cloud_x1"></div>
+            <div className="cloud">
+            <div className="cloud x1"></div>
             <div className="cloud x1_5"></div>
             <div className="cloud x2"></div>
             <div className="cloud x3"></div>
             <div className="cloud x4"></div>
             <div className="cloud x5"></div>
         </div>
-        <div className='c'>
-            <div className='_404'>404</div>
+        <div className="c">
+            <div className="_404">404</div>
             <hr className="horizontal-rule"/>
-            <div className='_1'>THE PAGE</div>
-            <div className='_2'>WAS NOT FOUND</div>
-            <a className="btn" href='/'>BACK TO MARS</a>
+            <div className="_1">THE PAGE</div>
+            <div className="_2">WAS NOT FOUND</div>
+            <a className="button" href='/'>BACK TO MARS</a>
         </div>
 
         </div>
