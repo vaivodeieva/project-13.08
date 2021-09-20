89598a6907bf9e89e01b688ed4325e3eaa95a019
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-20 12:54:34 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -36,7 +36,7 @@ function Header() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex flex row-reverse d-flex justify-content-center">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex flex row-reverse">
                 <div className="container-fluid">
                     <button onClick={toggleMainMenu} className={menuTogglerButtonClasses} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -45,12 +45,17 @@ function Header() {
                     <div className={mainMenuClasses} id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             <img className="home-picture animation2" src={ieva}/>
+                            
                             <NavLink className="nav-link home-circle" exact to="/">Home</NavLink>
-                            <li className="nav-item github-circle">
-                               <a className="nav-link" href="https://github.com/vaivodeieva">Github</a>
-                            </li>
+                            <span className="nav-item github-circle">
+                              <a className="nav-link" href="https://github.com/vaivodeieva">Github</a>
+                            </span>
+                            <span className="nav-item hobbies-circle">
                             <NavLink className="nav-link hobbies-circle" to="/hobbies">Hobbies</NavLink>
+                            </span>
+                            <span>
                             <NavLink className="nav-link contact-circle" to="/contact" >Contact</NavLink>
+                            </span>
                         </div>
                     </div>
                 </div>
