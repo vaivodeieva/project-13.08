e0f9a42f19fc940ec4909f81b7d6735809cbb71e
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-21 15:09:43 +0300

    update contact

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -4,40 +4,42 @@ import river from '../Assets/Images/river.jpg';
 function Contact() {
     return (
         <div className="contact-container">
-           <div className="row">
-               <div className="col-6">
-                  <img src={river} className="img-fluid"/>
-               </div>
-               <div className="col-6">
-               <h1>I'd love to hear from you</h1>
-               <h3>Like me on Facebook</h3>
-               <h3>Follow me on Twitter</h3>
-               <h3>Connect with me on LinkedIn</h3>
-               </div>
-           </div>
-           <div className="row">
-               <div className="col-12">
-                   
-                   <h2>Send me an email</h2>
-                   <div className="col-6">
-                       <div className="row">
-                          <div className="">
-                             Your name
-                          </div>
-                          <div className="col">
-                             Your email
-                          </div>
-                       </div>
-                      
-                   </div>
-                   <div className="col-6">
+            <div className="row">
+                <div className="col-6">
+                    <img src={river} className="img-fluid" />
+                </div>
+                <div className="col-6">
+                    <h1>I'd love to hear from you</h1>
+                    <h3>Like me on Facebook</h3>
+                    <h3>Follow me on Twitter</h3>
+                    <h3>Connect with me on LinkedIn</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <h2>Send me an email</h2>
+                    <div className="row">
 
-                   </div>
-               </div>
+                        <div className="col-6">
+                         <label for="name">Name</label>
+                         <input id="name"></input>
+                         <label for="email">Email</label>
+                         <input id="email"></input>
+                      </div>
+                      <div className="col-6">
+                           <label for="message">Message</label>
+                           <textarea id="message"></textarea>
+                      </div>
 
-           </div>
-        </div>
-    )
+                        </div>
+                        <div className="col-6">
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            )
 }
 
-export default Contact;
+            export default Contact;
