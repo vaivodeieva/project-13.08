527cdf614be8c3dac4c902682500e986ef745e26
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-22 11:24:21 +0300

    Update Contact.js

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -29,7 +29,7 @@ function Contact() {
                         <div className="mb-3">
                             <label for="exampleInputEmail1" className="form-label">Email address</label>
                             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            <div id="emailHelp" className="form-text">Your email will never be shared your with anyone else.</div>
                         </div>
 
                         <div className="form-floating">
