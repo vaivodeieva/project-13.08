56fafa5db57b858246cd2831d951d9a73718e127
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-22 09:44:47 +0300

    cleaning code

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,18 +1,17 @@
 import '../Assets/Css/Contact.css';
-import river from '../Assets/Images/river.jpg';
 
 function Contact() {
     return (
         <div className="contact-container">
             <div className="row">
                 <div className="col-6">
-                    <img src={river} className="img-fluid" />
                 </div>
                 <div className="col-6">
                     <h1>I'd love to hear from you</h1>
                     <h3>Like me on Facebook</h3>
                     <h3>Follow me on Twitter</h3>
                     <h3>Connect with me on LinkedIn</h3>
+
                 </div>
             </div>
             <div className="row">
