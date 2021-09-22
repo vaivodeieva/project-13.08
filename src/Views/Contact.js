a2fc32ea4b07785883dd006b8e25a3135c7b1fc0
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-09-22 10:53:04 +0300

    Update Contact.js

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -4,41 +4,34 @@ function Contact() {
     return (
         <div className="contact-container">
             <div className="row">
-                <div className="col-6">
+                <div className="col-12">
+                <h1>I'd love to hear from you</h1>
                 </div>
-                <div className="col-6">
-                    <h1>I'd love to hear from you</h1>
+
+            </div>
+            <div className="row">
+
+                <div className="col-4">
+                   
                     <h3>Like me on Facebook</h3>
                     <h3>Follow me on Twitter</h3>
                     <h3>Connect with me on LinkedIn</h3>
 
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
+
+                <div className="col-8">
                     <h2>Send me an email</h2>
-                    <div className="row">
-
-                        <div className="col-6">
-                         <label for="name">Name</label>
-                         <input id="name"></input>
-                         <label for="email">Email</label>
-                         <input id="email"></input>
-                      </div>
-                      <div className="col-6">
-                           <label for="message">Message</label>
-                           <textarea id="message"></textarea>
-                      </div>
-
-                        </div>
-                        <div className="col-6">
-
-                        </div>
-                    </div>
-
+                    <label for="name">Name</label>
+                    <input id="name"></input>
+                    <label for="email">Email</label>
+                    <input id="email"></input>
+                    <label for="message">Message</label>
+                    <textarea id="message"></textarea>
                 </div>
+
             </div>
-            )
+        </div>
+    )
 }
 
-            export default Contact;
+export default Contact;
