199b592dd1d94947b86ed6568a3dd4f7377dddbd
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-10-05 20:56:14 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -36,7 +36,7 @@ function Header() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex flex row-reverse">
+            <nav className="navbar navbar-expand-lg d-flex flex row-reverse">
                 <div className="container-fluid">
                     <button onClick={toggleMainMenu} className={menuTogglerButtonClasses} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
