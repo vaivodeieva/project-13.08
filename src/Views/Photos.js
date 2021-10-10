2e39914681ce27edd70e1a12fbf9556935eeafb9
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-10-10 15:00:51 +0300

    i lightgallery

--- src/Views/Photos.js
+++ src/Views/Photos.js
@@ -1,119 +1,29 @@
-import '../Assets/Css/Photos.css'
+import '../Assets/Css/Photos.css';
+import lightGallery from 'lightgallery';
+
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
 
 function Photos() {
 
-    
+    lightGallery(document.getElementById('lightgallery'), {
+        plugins: [lgZoom, lgThumbnail],
+        speed: 500,
+        licenseKey: 'your_license_key'
+        
+    });
+
     return (
         <div className="photos-container">
-             <div className="home">
-        <div className="container" style={{marginTop: '40px'}}>
-            <h2>Jquery LightGallery</h2>
-            <div className="demo-gallery">
-                <ul id="lightgallery" className="list-unstyled row">
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-sub-html="<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-1.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-2.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-13.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-4.jpg"/>
-                        </a>
-                    </li>
-    				<li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-sub-html="<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-1.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-2.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-13.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-4.jpg"/>
-                        </a>
-                    </li>
-    				<li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-sub-html="<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-2.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-1.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-2.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-13.jpg"/>
-                        </a>
-                    </li>
-    				<li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-sub-html="<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-4.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-1.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-2.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-13.jpg"/>
-                        </a>
-                    </li>
-    				<li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/4-1600.jpg" data-sub-html="<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-4.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/1-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-1.jpg"/>
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/2-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-2.jpg"/> 
-                        </a>
-                    </li>
-                    <li className="col-xs-6 col-sm-4 col-md-2 col-lg-2" data-responsive="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-src="https://sachinchoolur.github.io/lightgallery.js/static/img/13-1600.jpg" data-sub-html="<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>">
-                        <a href="">
-                            <img className="img-responsive" src="https://sachinchoolur.github.io/lightgallery.js/static/img/thumb-13.jpg"/>
-                        </a>
-                    </li>
-                </ul>
+            <div id="lightgallery">
+                <a href="img/img1.jpg" data-lg-size="1600-2400">
+                    <img alt=".." src="img/thumb1.jpg" />
+                </a>
+                <a href="img/img2.jpg" data-lg-size="1024-800">
+                    <img alt=".." src="img/thumb2.jpg" />
+                </a>
+               
             </div>
-        </div>
-        </div>
         </div>
     )
 }
