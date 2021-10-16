7ab44c4b17d2ec91e2a5705acae56ec1fa45987e
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-10-16 21:06:23 +0300

    add lightbox gallery to Photos

--- src/Views/Photos.js
+++ src/Views/Photos.js
@@ -5,6 +5,204 @@ function Photos() {
     return (
         <div>
 
+<div class="gallery">
+
+
+<div class="gallery__images-item one">
+  <a href="#1" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/yj1a_photo-1517394834181-95ed159986c7.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item two">
+  <a href="#2" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/lei_photo-1520986606214-8b456906c813.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item three">
+  <a href="#3" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/pkws_photo-1533204515132-035bc8d4128c.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item four">
+  <a href="#4" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/y8j8_photo-1523304867125-2293c498e08a.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item five">
+  <a href="#5" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/13y5_photo-1511473299613-ea38c5771c59.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item six">
+  <a href="#6" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/muyl_photo-1496320540980-6957e6d80966.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item seven">
+  <a href="#7" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/i3js_photo-1486994923624-fd50e5dc24c3.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item eight">
+  <a href="#8" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/yd1n_photo-1457267492735-d93fd34397d3.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+<div class="gallery__images-item nine">
+  <a href="#9" class="gallery__images-link">
+    <span class="overlay"></span>
+    <img src="https://uupload.ir/files/snsh_photo-1529266868593-483214d9d63c.jpeg" class="gallery__images-small" />
+  </a>
+</div>
+
+
+<div class="gallery__lightbox" id="1">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/yj1a_photo-1517394834181-95ed159986c7.jpeg" class="gallery__lightbox-image" />
+    <a href="#2" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="2">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/lei_photo-1520986606214-8b456906c813.jpeg" class="gallery__lightbox-image" />
+    <a href="#1" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#3" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="3">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/pkws_photo-1533204515132-035bc8d4128c.jpeg" class="gallery__lightbox-image" />
+    <a href="#2" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#4" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="4">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/y8j8_photo-1523304867125-2293c498e08a.jpeg" class="gallery__lightbox-image" />
+    <a href="#3" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#5" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="5">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/13y5_photo-1511473299613-ea38c5771c59.jpeg" class="gallery__lightbox-image" />
+    <a href="#4" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#6" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="6">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/muyl_photo-1496320540980-6957e6d80966.jpeg" class="gallery__lightbox-image" />
+    <a href="#5" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#7" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="7">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/i3js_photo-1486994923624-fd50e5dc24c3.jpeg" class="gallery__lightbox-image" />
+    <a href="#6" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#8" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="8">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/yd1n_photo-1457267492735-d93fd34397d3.jpeg" class="gallery__lightbox-image" />
+    <a href="#7" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a href="#9" class="next">
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
+</div>
+
+<div class="gallery__lightbox" id="9">
+  <div class="gallery__lightbox-content">
+    <a href="#" class="close">
+      ×
+    </a>
+    <img src="https://uupload.ir/files/snsh_photo-1529266868593-483214d9d63c.jpeg" class="gallery__lightbox-image" />
+    <a href="#8" class="back">
+      <i class="fas fa-chevron-left"></i>
+    </a>
+  </div>
+</div>
+</div>
+
+
             
         </div>
     )
